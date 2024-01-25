f663dcdf91e5cea7c6500d54deadf239a8c59755
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-01-25 20:38:49 +0530

    initialized the project

---
+++ src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+//import logoDog from '../../assets/logoDog.svg';
+//import logoDog from './assets/logoDog.svg'
+function Nav() {
+  return (
+    <header className=' bg-yellow-200  text-white'>
+        <div className="">
+            <div className="">
+              <img src="{logoDog}" alt="" className="" />
+              <h2 className="text-4xl font-semibold">perfect PawSitters</h2>
+            </div>
+
+            <div className="">
+             
+            </div>
+
+            {/* login/signup */}
+            <div className="">
+
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Nav
