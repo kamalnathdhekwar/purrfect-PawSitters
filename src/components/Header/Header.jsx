d1f6163adc5365567694524cb97ab61a24566994
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-01-30 23:43:41 +0530

    login signUp button & nav upadted

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,15 +18,9 @@ export default function Header() {
                     <div className="flex items-center lg:order-2">
                         <Link
                             to="/"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-7 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
-                            Log in
-                        </Link>
-                        <Link
-                            to="/"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Get started
+                            Login/Sign up
                         </Link>
                     </div>
                     
@@ -34,12 +28,12 @@ export default function Header() {
                         className=" justify-between   items-end"
                         id="mobile-menu-2"
                     >
-                        <ul className="flex  mt-4 font-semibold md:flex-row space-x-8  items-center text-lg">
+                        <ul className="flex  font-semibold md:flex-row space-x-8  items-center text-lg">
                             <li>
                                 <NavLink
                                 to="/"
                                     className={({isActive}) =>
-                                    `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-orange-700 underline   " :"text-gray-700"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-orange-700 underline   " :"text-white"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
@@ -49,7 +43,7 @@ export default function Header() {
                                 <NavLink
                                 to="/service"
                                     className={({isActive}) =>
-                                    `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-orange-700  underline  " :"text-gray-700"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-orange-700  underline  " :"text-white"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Services
@@ -59,7 +53,7 @@ export default function Header() {
                                 <NavLink
                                 to="/help"
                                     className={({isActive}) =>
-                                    `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-orange-700  underline " :"text-gray-700"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-orange-700  underline " :"text-white"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Help
@@ -69,7 +63,7 @@ export default function Header() {
                                 <NavLink
                                 to="/about"
                                     className={({isActive}) =>
-                                    `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-orange-700  underline " :"text-gray-700"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-orange-700  underline " :"text-white"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     About
